6cf280ff2ee60d4c59f430d337ffb08585f2a5f9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-09 10:49:25 -0600

    Add Seeder logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10);
+
+        $mensClothing = Category::factory()->create([
+            'name' => 'Mens Clothing'
+        ]);
+
+        $womensClothing = Category::factory()->create([
+            'name' => 'Womens Clothing'
+        ]);
+
+        Product::factory(20)->create([
+            'category_id' => $mensClothing->id
+        ]);
+
+        Product::factory(20)->create([
+            'category_id' => $womensClothing->id
+        ]);
     }
 }
