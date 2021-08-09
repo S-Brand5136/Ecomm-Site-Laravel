75442993a7b355e30b10f18278bb0b5c9b190c78
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-08 20:13:21 -0600

    Define craete_categories migration

--- database/migrations/2021_08_09_020157_create_categories.php
+++ database/migrations/2021_08_09_020157_create_categories.php
@@ -15,6 +15,8 @@ class CreateCategories extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
