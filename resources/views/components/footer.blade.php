12651b1009b20dcca117ced6567794edd75bdd4b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-10 15:47:54 -0600

    add basic footer styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,34 @@
 <footer class="footer mt-5">
-
+    <div class="d-flex flex-row justify-content-around">
+        <div class="mt-5 d-flex flex-column-justify-content-center">
+            <ul class="text-white">
+                <li>
+                    <h4>
+                        &copy; Ecom-Site: 2021
+                    </h4>
+                </li>
+                <li>
+                    <h5>Created By: Brandon Shemilt</h5>
+                </li>
+            </ul>
+        </div>
+        <div class="mt-5 d-flex flex-column-justify-content-center">
+            <ul class="text-white">
+                <li>
+                    <h4>
+                        Created Using:
+                    </h4>
+                </li>
+                <li>
+                    <p>Laravel</p>
+                </li>
+                <li>
+                    <p>Bootstrap</p>
+                </li>
+                <li>
+                    <p>SCSS</p>
+                </li>
+            </ul>
+        </div>
+    </div>
 </footer>
