03afed7e04be31342f3bbf89b86fb928efe1460a
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-10 16:07:31 -0600

    Add remaining navbar links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light pr-md-5 pl-md-5">
-    <a class="navbar-brand" href="#">Ecom-Site</a>
+    <a class="navbar-brand" href="/">Ecom-Site</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Home</a>
+                  <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">About Us</a>
+                  <a class="nav-link" href="/about">About Us</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Products</a>
+                  <a class="nav-link" href="/products">Products</a>
                 </li>
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -24,25 +24,25 @@
                   </div>
                 </li>
                 <li class="nav-item d-lg-none d-xl-none">
-                    <a href="#" class="nav-link">Login</a>
+                    <a href="/login" class="nav-link">Login</a>
                 </li>
                 <li class="nav-item d-lg-none d-xl-none">
-                    <a href="#" class="nav-link">Sign Up</a>
+                    <a href="/signup" class="nav-link">Sign Up</a>
                 </li>
                 <li class="nav-item d-lg-none d-xl-none">
-                  <a class="nav-link" href="#"><i class="fas text-dark fa-shopping-cart mr-2"></i>Cart</a>
+                  <a class="nav-link" href="/cart"><i class="fas text-dark fa-shopping-cart mr-2"></i>Cart</a>
                 </li>
             </ul>
         </div>
         <ul class="navbar-nav d-none d-lg-flex d-xl-flex d-md-none" >
             <li class="nav-item">
-                <a href="#" class="nav-link">Login</a>
+                <a href="/login" class="nav-link">Login</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">Sign Up</a>
+                <a href="/signup" class="nav-link">Sign Up</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-shopping-cart text-dark icon mr-2"></i>Cart</a>
+                <a href="/cart" class="nav-link"><i class="fas fa-shopping-cart text-dark icon mr-2"></i>Cart</a>
             </li>
         </ul>
 </nav>
