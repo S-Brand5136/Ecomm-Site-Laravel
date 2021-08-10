8ac25fe00cc9bfd00918e23ed678d84e89434263
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-10 16:06:18 -0600

    Add some basic styling to product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,3 +1,20 @@
 <x-layout>
-
+    <section class="container-fluid d-xs-block flex-row d-md-flex mt-5">
+        <div class="container text-align-center d-flex flex-column justify-content-around mb-5">
+            <h1 class="display-3 text-uppercase font-weight-bold">
+                <ins>{{ $product->name }}</ins>
+            </h1>
+            <div class="d-flex flex-column justify-content-around">
+                <h1 class="text-muted">{{ $product->description }}</h1>
+                <div class="d-flex flex-column">
+                    <h4 class="mt-3 font-weight-bold">Price: <span class="font-weight-bold"></span>${{ ($product->price) / 100 }}</h4>
+                    <h4 class="mt-3 font-weight-bold">Stock: <span class="font-weight-bold"></span>{{ ($product->quantity) }}</h4>
+                </div>
+            </div>
+            <a href="products/{{ $product->slug }}" class="btn btn-primary mt-2 p-3 {{ $product->quantity <= 0 ? 'disabled' : '' }} ">{{ $product->quantity <= 0 ? 'Out Of Stock' : 'Add To Card' }}</a>
+        </div>
+        <div class="ml-md-5 border-0">
+            <img src="{{ $product->imageURL }}" class="card-img img-fluid" alt="">
+        </div>
+    </section>
 </x-layout>
