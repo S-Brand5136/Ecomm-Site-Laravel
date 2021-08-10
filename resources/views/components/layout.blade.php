f8c09e2d3bc731bd9fe3b205bc1bb3ba7324fa17
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-10 13:47:53 -0600

    Update navbar styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
         <title>Ecomm-Site</title>
     </head>
     <body>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,31 +3,46 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Products</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Categories
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Mens Clothing</a>
-                <a class="dropdown-item" href="#">Womens Clothing</a>
-              </div>
-            </li>
-          </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Mens Clothing</a>
+                    <a class="dropdown-item" href="#">Womens Clothing</a>
+                  </div>
+                </li>
+                <li class="nav-item d-lg-none d-xl-none">
+                    <a href="#" class="nav-link">Login</a>
+                </li>
+                <li class="nav-item d-lg-none d-xl-none">
+                    <a href="#" class="nav-link">Sign Up</a>
+                </li>
+                <li class="nav-item d-lg-none d-xl-none">
+                  <a class="nav-link" href="#"><i class="fas text-dark fa-shopping-cart mr-2"></i>Cart</a>
+                </li>
+            </ul>
         </div>
-    </div>
-    <div>
-
-    </div>
-  </nav>
+        <ul class="navbar-nav d-none d-lg-flex d-xl-flex d-md-none" >
+            <li class="nav-item">
+                <a href="#" class="nav-link">Login</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Sign Up</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i class="fas fa-shopping-cart text-dark icon mr-2"></i>Cart</a>
+            </li>
+        </ul>
+</nav>
