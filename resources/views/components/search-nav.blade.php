7c066797bddbc46655084de48a98262e1b3d42fc
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-13 16:14:41 -0600

    Add base products search form

---
+++ resources/views/components/search-nav.blade.php
@@ -0,0 +1,17 @@
+<div class="container p-md-5 p-3">
+    <h2>Search our Clothing Line!</h2>
+    <form action="GET">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-xs-6 col-md-6 mt-3">
+                <select class="custom-select">
+                    <option value="1">Mens Clothing</option>
+                    <option value="2">Womens Clothing</option>
+                </select>
+            </div>
+
+            <div class="col-xs-6 col-md-6 mt-3">
+                <input type="text" placeholder="Search.." class="form-control" id="search" aria-describedby="search">
+            </div>
+        </div>
+    </form>
+</div>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <div class="container-fluid p-5 max-height w-100">
+        <x-search-nav />
         <div class="row justify-content-center">
             @foreach ($products as $product)
                 <x-product-card :product="$product" class="mr-md-5 col-xs-2 col-offset-2 " />
