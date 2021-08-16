18149219ffdd5cef5c6e9ea9ea9cfcfaa9346718
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-15 19:46:25 -0600

    Add register login to controller and user Model

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -13,5 +14,14 @@ class RegisterController extends Controller
 
     public function store()
     {
+        $attributes = request()->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'max:255', 'email'],
+            'password' => ['required', 'max:255', 'min:7']
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
     }
 }
