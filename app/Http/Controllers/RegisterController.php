d8a8d28789172f4f4487a09d03d62f88dc83d52d
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-25 19:36:05 -0600

    Remove flash message on register redirect

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -25,6 +25,6 @@ class RegisterController extends Controller
 
         Auth::login($user);
 
-        return redirect('/')->with('success', 'Your account has been created');
+        return redirect('/');
     }
 }
