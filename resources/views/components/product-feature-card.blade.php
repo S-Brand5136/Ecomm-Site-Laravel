c237c2bc9d4f974070be1de2c7b348449de8dbf7
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-25 19:23:10 -0600

    Add conditional variable for button text

--- resources/views/components/product-feature-card.blade.php
+++ resources/views/components/product-feature-card.blade.php
@@ -1,6 +1,13 @@
 @props(['product'])
 
-<article {{ $attributes->merge(['class' => "container-fluid d-xs-block d-md-flex mt-5"]) }}>
+@php
+$buttonText = 'Learn More';
+if (strpos(Request::url(), 'products')) {
+    $buttonText = 'Add To Cart';
+}
+@endphp
+
+<article {{ $attributes->merge(['class' => 'container-fluid d-xs-block d-md-flex mt-5']) }}>
     <div class="container text-align-center d-flex flex-column justify-content-around mb-5">
         <h1 class="display-1">
             {{ $product->name }}
@@ -8,12 +15,16 @@
         <div class="d-flex flex-column justify-content-around">
             <h3 class="text-muted display-4">{{ $product->description }}</h3>
             <div class="d-flex flex-column">
-                <h4 class="mt-3 font-weight-bold">Price: <span class="font-weight-bold"></span>${{ ($product->price) / 100 }}</h4>
-                <h4 class="mt-3 font-weight-bold">Stock: <span class="font-weight-bold"></span>{{ ($product->quantity) }}</h4>
+                <h4 class="mt-3 font-weight-bold">Price: <span
+                        class="font-weight-bold"></span>${{ $product->price / 100 }}</h4>
+                <h4 class="mt-3 font-weight-bold">Stock: <span
+                        class="font-weight-bold"></span>{{ $product->quantity }}</h4>
             </div>
-            <h4 class="font-weight-bold"><small class="text-muted">Posted: {{ $product->created_at->diffForHumans() }}</small></h4>
+            <h4 class="font-weight-bold"><small class="text-muted">Posted:
+                    {{ $product->created_at->diffForHumans() }}</small></h4>
         </div>
-        <a href="products/{{ $product->slug }}" class="btn btn-primary mt-2 p-3 {{ $product->quantity <= 0 ? 'disabled' : '' }} ">{{ $product->quantity <= 0 ? 'Out Of Stock' : 'Learn More' }}</a>
+        <a href="products/{{ $product->slug }}"
+            class="btn btn-primary mt-2 p-3 {{ $product->quantity <= 0 ? 'disabled' : '' }} ">{{ $product->quantity <= 0 ? 'Out Of Stock' : $buttonText }}</a>
     </div>
     <div class="ml-md-5 border-0">
         <img src="{{ $product->imageURL }}" class="card-img img-fluid" alt="">
