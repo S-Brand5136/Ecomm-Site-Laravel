4102d55f14ad6855c68cd2f83b6843c51f567721
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-28 16:50:37 -0600

    Add html and styling for about page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,3 +1,58 @@
 <x-layout>
-    <h1>Hello World</h1>
+    <div class="max-height">
+        <header class="card container-fluid mt-5 px-5">
+            <h1 class="mt-2">Ecom-Site</h1>
+            <p class="font-weight-bold ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quo ullam dicta temporibus qui molestiae,
+                alias excepturi. Itaque laborum, explicabo necessitatibus dolor distinctio earum velit ipsa minima cum?
+                Debitis, vero.
+            </p>
+        </header>
+
+        <main class="container-fluid mt-5 px-lg-5 px-sm-1">
+            <section class="card p-5">
+                <div class="row justify-content-end">
+                    <div class="col-lg-6 col-sm-12">
+                        <p class="lead">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, mollitia sed. Ullam rem
+                            commodi
+                            ea
+                            impedit eligendi sint, non corrupti minus aperiam quidem natus voluptatibus neque voluptate
+                            fuga
+                            asperiores.
+                            Exercitationem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
+                            doloremque,
+                            quibusdam
+                            similique aspernatur quaerat expedita optio, qui facere odit laborum rerum blanditiis
+                            eveniet
+                            consectetur ex
+                            quos exercitationem aperiam iste ullam. Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit.
+                            Nihil
+                            sunt
+                            quas quo odit temporibus nam quam autem, nesciunt eaque atque, perspiciatis illum, voluptate
+                            optio
+                            vel.
+                            Quia
+                            eos molestiae quod. Inventore
+                        </p>
+                        <div>
+                            <p>Check out our socials</p>
+                            <a href="#"><i class="bi bi-instagram mr-2 text-primary h4"></i></a>
+                            <a href="#"><i class="bi bi-facebook mr-2 text-primary h4"></i></a>
+                            <a href="#"><i class="bi bi-linkedin mr-2 text-primary h4"></i></a>
+                        </div>
+                        <p class="text-mute text-small">
+                            Ecom-site copyright 2021
+                        </p>
+
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <img class="img-fluid rounded" src="{{ asset('/images/about-image.jpg') }}" alt="">
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+
 </x-layout>
