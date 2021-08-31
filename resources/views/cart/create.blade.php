023bd3d6497b687ae9f6d7000f17ffcc4e66dcff
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-30 21:39:12 -0600

    Add basic html to shopping cart page

--- resources/views/cart/create.blade.php
+++ resources/views/cart/create.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
+    <main class="max-height container mt-5">
+        <header class="container text-center mt-5">
+            <h1 class="font-weight-bold display-4">Check-Out</h1>
+        </header>
 
+        <section class="row justify-content-center mt-5">
+            <x-shopping-cart-table />
+            <x-shopping-cart-total />
+        </section>
+    </main>
 </x-layout>
