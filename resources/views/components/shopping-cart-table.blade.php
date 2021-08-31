ef77a8fff969da3c5e39dd1b64ba241f24b57fce
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-30 21:38:59 -0600

    Update shopping cart table markup

--- resources/views/components/shopping-cart-table.blade.php
+++ resources/views/components/shopping-cart-table.blade.php
@@ -1,20 +1,29 @@
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col"></th>
-            <th scope='col'>Product Name</th>
-            <th scope='col'>Quantity</th>
-            <th scope='col'>Price</th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">image</th>
-            <th>T-Shirt</th>
-            <th>1</th>
-            <th>$19.99</th>
-            <th><button class="badge border-0 badge-danger text-wrap">X</button></th>
-        </tr>
-    </tbody>
-</table>
+<div class="card col-sm-12 col-md-7">
+    <div class="card-body">
+        <div class="card-title font-weight-bolder">Shopping Cart</div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope='col'>Product Name</th>
+                    <th scope='col'>Quantity</th>
+                    <th scope='col'>Price</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">small image</th>
+                    <th>T-Shirt</th>
+                    <th>1</th>
+                    <th>$19.99</th>
+                    <th><button class="badge border-0 badge-danger text-wrap">X</button></th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="row border-top py-3  px-4 align-items-end">
+        <a href="/products" class="btn btn-light">
+            < Countinue Shopping</a>
+    </div>
+</div>
