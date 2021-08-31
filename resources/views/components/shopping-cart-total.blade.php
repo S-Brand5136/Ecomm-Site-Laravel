48e5e389936dae2662268e7781bc3d2d31a4596b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-30 21:38:44 -0600

    Create seperate component for shopping Cart totals

---
+++ resources/views/components/shopping-cart-total.blade.php
@@ -0,0 +1,26 @@
+<div class="card col-sm-12 offset-md-1 col-md-4">
+    <div class="card-title border-bottom font-weight-bolder mt-3">
+        <h3>Total Amount Due</h3>
+    </div>
+    <div class="card-body">
+        <div class="row p-2">
+            <p class="col-sm-6 h5 font-weight-bold">Sub-total:</p>
+            <p class="col-sm-6 h5 font-weight-bold">$19.99</p>
+        </div>
+
+        <div class="row mt-2 p-2 border-top">
+            <p class="col-sm-6 h5 font-weight-bold">Tax:</p>
+            <p class="col-sm-6 h5 font-weight-bold">$4.64</p>
+        </div>
+
+        <div class="row mt-2 p-2 border-top">
+            <p class="col-sm-6 h5 font-weight-bold">Total:</p>
+            <p class="col-sm-6 h5 font-weight-bold">$24.63</p>
+        </div>
+
+        <div class="row mt-2 p-2 border">
+            <h5 class="col-sm-12">Payment Options</h5>
+            <button class="col-sm-12 mt-4 btn btn-primary">PlaceHolder</button>
+        </div>
+    </div>
+</div>
