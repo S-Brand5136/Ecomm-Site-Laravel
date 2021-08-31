ea57113d1cab33092e16613b98aac167b0ae94b5
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-30 21:03:09 -0600

    Add shopping cart component

---
+++ resources/views/components/shopping-cart-table.blade.php
@@ -0,0 +1,20 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope='col'>Product Name</th>
+            <th scope='col'>Quantity</th>
+            <th scope='col'>Price</th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">image</th>
+            <th>T-Shirt</th>
+            <th>1</th>
+            <th>$19.99</th>
+            <th><button class="badge border-0 badge-danger text-wrap">X</button></th>
+        </tr>
+    </tbody>
+</table>
