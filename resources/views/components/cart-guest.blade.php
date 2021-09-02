6ed87f2720b83c7651caa23747331f49c5664973
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-09-01 21:43:03 -0600

    Add basic html to guest cart component

---
+++ resources/views/components/cart-guest.blade.php
@@ -0,0 +1,10 @@
+<div class="card col-sm-12">
+    <div class="card-body">
+        <div class="card-title font-weight-bolder">Guest</div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="text-center">Log in to get access to a cart!</h3>
+            </div>
+        </div>
+    </div>
+</div>
